cddae4cec8df586d2999d7b3dda70a14f102aac4
Sayo-Exo <75951527+Sayo-Exo@users.noreply.github.com>
2022-05-05 10:25:01 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-paragraphs = document.querySelectorAll('p')
-headers = document.querySelectorAll('h1')
+const paragraphs = document.querySelectorAll('p');
+const headers = document.querySelectorAll('h1');
 
-for (int i = 0; i < paragraphs.length; i++) {
+for (let i = 0; i < paragraphs.length; i++) {
     const p = paragraphs[i];
     let text = p.innerHTML;
-    text = form(text, "leader", "leader")
-    text = form(text, "Ancient Egypt", "egypt")
-    text = form(text, "Egypt", "egypt")
-    text = form(text, "Mesopotamia", "meso")
+    text = form(text, "leader", "leader");
+    text = form(text, "Ancient Egypt", "egypt");
+    text = form(text, "Egypt", "egypt");
+    text = form(text, "Mesopotamia", "meso");
     p.innerHTML = text;
 }
 
 function form(text, select, c) {
-    return text.replaceAll(select, `<span class="${c}">${select}</span>`)
+    return text.replaceAll(select, `<span class="${c}">${select}</span>`);
 }
